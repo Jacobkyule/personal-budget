48b2d51de41c05258963abdc9db9e71e9de0a86f
Jacob Kyule <jacobkyule76@gmail.com>
2023-12-04 11:55:09 +0300

    initialization of project

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+
+module.exports = app;
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, ()=>{
+    console.log(`Server is up and running on port ${PORT}`);
+})
+
+
+app.get('/', (req, res, next)=>{
+    res.send('Hello World');
+})
